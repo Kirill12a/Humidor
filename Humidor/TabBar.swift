b8ddbfd49387f1a77e157e78d6ce81a2690f25f9
Kirill <kirilldrozd12@gmail.com>
2022-01-22 23:00:58 +0300

    Подгрузка экрана через код. И добавил экран TabBar

---
+++ Humidor/TabBar.swift
@@ -0,0 +1,46 @@
+//
+//  TabBar.swift
+//  Humidor
+//
+//  Created by Kirill Drozdov on 22.01.2022.
+//
+
+import UIKit
+
+class TabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+      view.backgroundColor = .systemBackground
+         UITabBar.appearance().barTintColor = .systemBackground
+         tabBar.tintColor = .label
+//      home.selectedIndex = 1
+         setupVCs()
+        // Do any additional setup after loading the view.
+    }
+
+  fileprivate func createNavController(for rootViewController: UIViewController,
+                                                  title: String,
+                                                  image: UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        navController.navigationBar.prefersLargeTitles = true
+//        rootViewController.navigationItem.title = title
+        return navController
+    }
+
+
+  func setupVCs() {
+        viewControllers = [
+            createNavController(for: ViewController(), title: NSLocalizedString("Хьюмидор", comment: ""), image: UIImage(systemName: "shippingbox")!),
+            createNavController(for: HistoryViewController(), title: NSLocalizedString("Банты", comment: ""), image: UIImage(named:"sigar")!),
+            createNavController(for: ProfileViewController(), title: NSLocalizedString("Профиль", comment: ""), image: UIImage(systemName: "person")!)
+        ]
+
+    }
+
+      }
+
